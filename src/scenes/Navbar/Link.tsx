c32c8e49d05c12a81d17247b5b640f3676fe6eb4
mariosknl <mariosknl@outlook.com>
2022-12-26 11:58:21 +0200

    Navbar Completed + Enums created

--- src/scenes/Navbar/Link.tsx
+++ src/scenes/Navbar/Link.tsx
@@ -1,13 +1,15 @@
-import AnchorLink from "react-anchor-link-smooth-scroll"
+import { SelectedPage } from "@/shared/types";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   page: string;
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
-}
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Link = ({page, selectedPage, setSelectedPage}: Props) => {
-  const lowerCasePage = page.toLowerCase().replace(/ /g, "")
+const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
+
   return (
     <AnchorLink
       className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""}
@@ -18,7 +20,7 @@ const Link = ({page, selectedPage, setSelectedPage}: Props) => {
     >
       {page}
     </AnchorLink>
-  )
-}
+  );
+};
 
-export default Link
+export default Link;
